1f59992717d62a5264af62b40ca07239ef5a00d7
alenka-byte <alenkaivas6@gmail.com>
2026-04-29 17:47:02 +0500

    Add files via upload

---
+++ lab07/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(78, 17);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(150, 27);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.Value = new decimal(new int[] { 10, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(45, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Дней";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(410, 54);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(718, 384);
+            chart1.TabIndex = 2;
+            chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1034, 17);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 3;
+            button1.Text = "Закрыть";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(234, 17);
+            button2.Name = "button2";
+            button2.Size = new Size(156, 29);
+            button2.TabIndex = 4;
+            button2.Text = "Прогнозирование";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(27, 54);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(377, 384);
+            listBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1140, 656);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(chart1);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+    }
+}
---
+++ lab07/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+
+        // 0 - ясно, 1 - облачно, 2 - пасмурно
+        int currentState = 0;
+        double[,] Lambda = new double[,]
+        {
+            { 0.7, 0.2, 0.1 },
+            { 0.1, 0.4, 0.5 },
+            { 0.6, 0.1, 0.3 }
+        };
+
+        string[] stateNames = { "Ясно", "Облачно", "Пасмурно" };
+ 
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Series.Clear();
+            chart1.Series.Add("Погода");
+            chart1.Series[0].ChartType = SeriesChartType.StepLine;
+            chart1.Series[0].BorderWidth = 3;
+            chart1.Series[0].Color = System.Drawing.Color.DarkBlue;
+
+            chart1.ChartAreas[0].AxisX.Title = "Время (дни)";
+            chart1.ChartAreas[0].AxisY.Title = "Состояние погоды";
+            chart1.ChartAreas[0].AxisY.Minimum = 0;
+            chart1.ChartAreas[0].AxisY.Maximum = 2;
+            chart1.ChartAreas[0].AxisY.Interval = 1;
+
+            chart1.ChartAreas[0].AxisY.CustomLabels.Clear();
+            chart1.ChartAreas[0].AxisY.CustomLabels.Add(-0.5, 0.5, "Ясно");
+            chart1.ChartAreas[0].AxisY.CustomLabels.Add(0.5, 1.5, "Облачно");
+            chart1.ChartAreas[0].AxisY.CustomLabels.Add(1.5, 2.5, "Пасмурно");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        double GetExpRV(double lambda)
+        {
+            if (lambda <= 0) return double.PositiveInfinity; 
+            return -Math.Log(random.NextDouble()) / lambda;
+        }
+
+        int GetNextState(int state)
+        {
+            double sum = 0;
+            for (int j = 0; j < 3; j++)
+                if (j != state)
+                    sum += Lambda[state, j];
+
+            double a = random.NextDouble() * sum;
+            double s = 0;
+            for (int j = 0; j < 3; j++)
+            {
+                if (j == state) continue;
+                s += Lambda[state, j];
+                if (a <= s)
+                    return j;
+            }
+            return state;
+        }
+
+        // Вычисление стационарного распределения (теоретического)
+        private double[] GetStationaryDistribution()
+        {
+            double λ01 = Lambda[0, 1], λ02 = Lambda[0, 2];
+            double λ10 = Lambda[1, 0], λ12 = Lambda[1, 2];
+            double λ20 = Lambda[2, 0], λ21 = Lambda[2, 1];
+            double out0 = λ01 + λ02; 
+            double out1 = λ10 + λ12; 
+            double out2 = λ20 + λ21; 
+
+            double coeff = (out1 * out0 - λ10 * λ01) / (out1 * λ20 + λ10 * λ21);
+            double π2_over_π0 = coeff;
+            double π1_over_π0 = (out0 - λ20 * coeff) / λ10;
+
+            double sumCoef = 1 + π1_over_π0 + π2_over_π0;
+            double π0 = 1.0 / sumCoef;
+            double π1 = π1_over_π0 * π0;
+            double π2 = π2_over_π0 * π0;
+
+            return new double[] { π0, π1, π2 };
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double totalDays = (double)numericUpDown1.Value; 
+
+            listBox1.Items.Clear();;
+
+            double[] Dur = new double[3];   // суммарное время в каждом состоянии
+            double t = 0;                   // текущее время
+            currentState = 0;               // начальное состояние
+
+            int[,] trans = new int[3, 3];
+
+            chart1.Series[0].Points.Clear();
+            chart1.Series[0].Points.AddXY(0, currentState);
+
+            int steps = 0; 
+
+            while (t < totalDays) 
+            {
+                // Суммарная интенсивность выхода из текущего состояния
+                double lambdaSum = 0;
+                for (int j = 0; j < 3; j++)
+                    if (j != currentState)
+                        lambdaSum += Lambda[currentState, j];
+
+                double dt = GetExpRV(lambdaSum);
+                if (t + dt >= totalDays)
+                {
+                    // Оставшееся время доживаем в текущем состоянии
+                    double r = totalDays - t;
+                    Dur[currentState] += r;
+                    t = totalDays;
+                    chart1.Series[0].Points.AddXY(totalDays, currentState);
+                    break;
+                }
+                else
+                {
+                    // Полный интервал в текущем состоянии
+                    Dur[currentState] += dt;
+                    t += dt;
+                    chart1.Series[0].Points.AddXY(t, currentState);
+
+                    // Переход в новое состояние
+                    int nextState = GetNextState(currentState);
+                    trans[currentState, nextState]++;
+                    currentState = nextState;
+                    steps++;
+                }
+            }
+
+            // Эмпирические вероятности
+            double[] empirical = new double[3];
+            for (int i = 0; i < 3; i++)
+                empirical[i] = Dur[i] / totalDays;
+
+            // Теоретическое стационарное распределение
+            double[] theoretical = GetStationaryDistribution();
+            double[] error = new double[3];
+            for (int i = 0; i < 3; i++)
+                error[i] = Math.Abs(empirical[i] - theoretical[i]);
+
+            listBox1.Items.Add("Сравнение вероятностей");
+            for (int i = 0; i < 3; i++)
+            {
+                listBox1.Items.Add($"{stateNames[i]}:");
+                listBox1.Items.Add($"  Эмпирическая: {empirical[i]:F6}");
+                listBox1.Items.Add($"  Теоретическая: {theoretical[i]:F6}");
+                listBox1.Items.Add($"  Ошибка:       {error[i]:F6}");
+            }
+            listBox1.Items.Add("");
+            listBox1.Items.Add($"Количество переходов: {steps}");
+            listBox1.Items.Add("");
+
+            string fileName = "weather_statistics.csv";
+            using (StreamWriter sw = new StreamWriter(fileName, false, System.Text.Encoding.UTF8))
+            {
+                sw.WriteLine("Состояние;Время (дни);Эмпирическая вероятность;Теоретическая вероятность;Абсолютная ошибка");
+                for (int i = 0; i < 3; i++)
+                {
+                    sw.WriteLine($"{stateNames[i]};{Dur[i]:F4};{empirical[i]:F6};{theoretical[i]:F6};{error[i]:F6}");
+                }
+                sw.WriteLine();
+                sw.WriteLine("Переходы (из -> в):");
+                sw.WriteLine("Из\\В;Ясно;Облачно;Пасмурно");
+                for (int i = 0; i < 3; i++)
+                {
+                    sw.Write($"{stateNames[i]};");
+                    for (int j = 0; j < 3; j++)
+                        sw.Write($"{trans[i, j]};");
+                    sw.WriteLine();
+                }
+                sw.WriteLine();
+                sw.WriteLine($"Количество переходов: {steps}");
+            }
+
+            listBox1.Items.Add($"Статистика сохранена в файл: {fileName}");
+        }
+    }
+}
