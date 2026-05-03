5c114916ddc75f47832152a56277a29ca5918e42
alenka-byte <alenkaivas6@gmail.com>
2026-05-03 18:21:26 +0700

    Update Form1.cs

--- lab07/Form1.cs
+++ lab07/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -13,12 +13,7 @@ namespace lab7
 
         // 0 - ясно, 1 - облачно, 2 - пасмурно
         int currentState = 0;
-        double[,] Lambda = new double[,]
-        {
-            { 0.7, 0.2, 0.1 },
-            { 0.1, 0.4, 0.5 },
-            { 0.6, 0.1, 0.3 }
-        };
+        double[,] Lambda = new double[3, 3];
 
         string[] stateNames = { "Ясно", "Облачно", "Пасмурно" };
  
@@ -62,24 +57,27 @@ namespace lab7
 
         int GetNextState(int state)
         {
-            double sum = 0;
+            double[] sum = new double[] { 0, 0, 0};
             for (int j = 0; j < 3; j++)
                 if (j != state)
-                    sum += Lambda[state, j];
+                    sum[j] = -Lambda[state, j] / Lambda[state, state];
+                else
+                    sum[j] = 0;
+                    
 
-            double a = random.NextDouble() * sum;
+            double a = random.NextDouble();
             double s = 0;
             for (int j = 0; j < 3; j++)
             {
                 if (j == state) continue;
-                s += Lambda[state, j];
+                s += sum[j];
                 if (a <= s)
                     return j;
             }
             return state;
         }
 
-        // Вычисление стационарного распределения (теоретического)
+        // Вычисление теоретического стационарного распределения
         private double[] GetStationaryDistribution()
         {
             double λ01 = Lambda[0, 1], λ02 = Lambda[0, 2];
@@ -102,16 +100,31 @@ namespace lab7
         }
         private void button2_Click(object sender, EventArgs e)
         {
-            double totalDays = (double)numericUpDown1.Value; 
+            double totalDays = (double)numericUpDown1.Value;
+            Lambda[0, 0] = (double)n00.Value;
+            Lambda[0, 1] = (double)n01.Value;
+            Lambda[0, 2] = (double)n02.Value;
+            Lambda[1, 0] = (double)n10.Value;
+            Lambda[1, 1] = (double)n11.Value;
+            Lambda[1, 2] = (double)n12.Value;
+            Lambda[2, 0] = (double)n20.Value;
+            Lambda[2, 1] = (double)n21.Value;
+            Lambda[2, 2] = (double)n22.Value;
+            listBox1.Items.Clear();
+            if (Lambda[0, 0] > 0 && Lambda[1, 1] > 0 && Lambda[2, 2] > 0)
+            {
 
-            listBox1.Items.Clear();;
-
-            double[] Dur = new double[3];   // суммарное время в каждом состоянии
-            double t = 0;                   // текущее время
-            currentState = 0;               // начальное состояние
+            }
+            double[] Dur = new double[3];   
+            double t = 0;
+            double[] theoretical = GetStationaryDistribution();
+            int minIndex = 0;
+            for (int i = 1; i < 3; i++)
+                if (theoretical[i] > theoretical[minIndex])
+                    minIndex = i;
+            currentState = minIndex;
 
             int[,] trans = new int[3, 3];
-
             chart1.Series[0].Points.Clear();
             chart1.Series[0].Points.AddXY(0, currentState);
 
@@ -119,7 +132,6 @@ namespace lab7
 
             while (t < totalDays) 
             {
-                // Суммарная интенсивность выхода из текущего состояния
                 double lambdaSum = 0;
                 for (int j = 0; j < 3; j++)
                     if (j != currentState)
@@ -128,7 +140,6 @@ namespace lab7
                 double dt = GetExpRV(lambdaSum);
                 if (t + dt >= totalDays)
                 {
-                    // Оставшееся время доживаем в текущем состоянии
                     double r = totalDays - t;
                     Dur[currentState] += r;
                     t = totalDays;
@@ -137,12 +148,10 @@ namespace lab7
                 }
                 else
                 {
-                    // Полный интервал в текущем состоянии
                     Dur[currentState] += dt;
                     t += dt;
                     chart1.Series[0].Points.AddXY(t, currentState);
 
-                    // Переход в новое состояние
                     int nextState = GetNextState(currentState);
                     trans[currentState, nextState]++;
                     currentState = nextState;
@@ -150,13 +159,11 @@ namespace lab7
                 }
             }
 
-            // Эмпирические вероятности
             double[] empirical = new double[3];
             for (int i = 0; i < 3; i++)
                 empirical[i] = Dur[i] / totalDays;
 
-            // Теоретическое стационарное распределение
-            double[] theoretical = GetStationaryDistribution();
+            
             double[] error = new double[3];
             for (int i = 0; i < 3; i++)
                 error[i] = Math.Abs(empirical[i] - theoretical[i]);
