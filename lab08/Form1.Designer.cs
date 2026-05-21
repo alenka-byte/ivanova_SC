76ce0ff253bcaf249b57e2cef703c196260c5f05
alenka-byte <alenkaivas6@gmail.com>
2026-05-21 13:50:11 +0500

    Add files via upload

---
+++ lab08/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            button1 = new Button();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            label2 = new Label();
+            numericUpDown2 = new NumericUpDown();
+            chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            label3 = new Label();
+            numericUpDown3 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(694, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 0;
+            button1.Text = "Закрыть";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(99, 12);
+            numericUpDown1.Maximum = new decimal(new int[] { -727379968, 232, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(73, 27);
+            numericUpDown1.TabIndex = 1;
+            numericUpDown1.Value = new decimal(new int[] { 1000, 0, 0, 0 });
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(54, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Время";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(195, 16);
+            label2.Name = "label2";
+            label2.Size = new Size(188, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Параметр интенсивности";
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.DecimalPlaces = 5;
+            numericUpDown2.Location = new Point(389, 12);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(119, 27);
+            numericUpDown2.TabIndex = 4;
+            numericUpDown2.Value = new decimal(new int[] { 5, 0, 0, 0 });
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            chart1.Location = new Point(310, 47);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.IsVisibleInLegend = false;
+            series1.Name = "Series1";
+            series2.ChartArea = "ChartArea1";
+            series2.Name = "Series2";
+            chart1.Series.Add(series1);
+            chart1.Series.Add(series2);
+            chart1.Size = new Size(478, 391);
+            chart1.TabIndex = 5;
+            chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(594, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(94, 29);
+            button2.TabIndex = 6;
+            button2.Text = "Запуск";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(25, 117);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(261, 224);
+            listBox1.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(43, 49);
+            label3.Name = "label3";
+            label3.Size = new Size(37, 20);
+            label3.TabIndex = 9;
+            label3.Text = "Шаг";
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(99, 45);
+            numericUpDown3.Maximum = new decimal(new int[] { -727379968, 232, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(73, 27);
+            numericUpDown3.TabIndex = 8;
+            numericUpDown3.Value = new decimal(new int[] { 100, 0, 0, 0 });
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(numericUpDown3);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(chart1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)chart1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private Label label2;
+        private NumericUpDown numericUpDown2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private Button button2;
+        private ListBox listBox1;
+        private Label label3;
+        private NumericUpDown numericUpDown3;
+    }
+}
---
+++ lab08/Form1.cs
@@ -0,0 +1,111 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace lab8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        Random random = new Random();
+        private double Factorial(int n)
+        {
+            double result = 1;
+
+            for (int i = 2; i <= n; i++)
+                result *= i;
+
+            return result;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            chart1.Series[0].Points.Clear();
+            chart1.Series[1].Points.Clear();
+            double T = (double)numericUpDown1.Value;
+            double lam = (double)numericUpDown2.Value;
+            int n = (int)numericUpDown3.Value;
+            int s = 0;
+            List<int> x_history = new List<int>();
+            for (int exp = 0; exp < n; exp++)
+            {
+                double t = 0;
+                int x = 0;
+                while (t < T)
+                {
+                    double r = random.NextDouble();
+                    double dt = -Math.Log(1 - r) / lam;
+                    t += dt;
+                    if (t <= T)
+                        x++;
+                    else
+                        s += x;
+                }
+                x_history.Add(x);
+            }
+            
+            int maxReq = x_history.Max();
+            int[] counts = new int[maxReq+ 1];
+            foreach (int value in x_history)
+            {
+                counts[value]++;
+            }
+            double[] probabilities = new double[maxReq + 1];
+            for (int i = 0; i <= maxReq; i++)
+            {
+                probabilities[i] = (double)counts[i] / n;
+            }
+
+            chart1.Series[0].Name = "Эмпирическое";
+            chart1.Series[1].Name = "Теоретическое";
+            chart1.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
+            chart1.Series[0].BorderWidth = 1; 
+            chart1.Series[0].Color = System.Drawing.Color.Blue; 
+            chart1.Series[0].Name = "Эмпирическое распределение";
+            chart1.Series[1].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            chart1.Series[1].BorderWidth = 3; 
+            chart1.Series[1].Color = System.Drawing.Color.Red; 
+            chart1.Series[1].Name = "Теоретическое распределение (экспоненциальное)";
+            chart1.ChartAreas[0].AxisX.Title = "Время"; 
+            chart1.ChartAreas[0].AxisY.Title = "Плотность вероятности";
+
+            for (int i = 0; i <= maxReq; i++)
+            {
+                chart1.Series[0].Points.AddXY(i, probabilities[i]);
+            }
+            double a = lam * T;
+            int maxX = x_history.Max();
+            for (int k = 0; k <= maxX; k++)
+            {
+                double p = Math.Pow(a, k) * Math.Exp(-a) / Factorial(k);
+                chart1.Series[1].Points.AddXY(k, p);
+            }
+            double empMean = x_history.Average();
+            double sum = 0;
+            foreach (int value in x_history)
+            {
+                double diff = value - empMean;
+                sum += diff * diff;
+            }
+            double empVar = sum / x_history.Count;
+            double theorMean = lam * T;
+            double theorVar = lam * T;
+            listBox1.Items.Add($"Число запросов за время T = {s}");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("ЭМПИРИЧЕСКИЕ ХАРАКТЕРИСТИКИ");
+            listBox1.Items.Add($"Среднее: {empMean:F4}");
+            listBox1.Items.Add($"Дисперсия: {empVar:F4}");
+            listBox1.Items.Add("");
+            listBox1.Items.Add("ТЕОРЕТИЧЕСКИЕ ХАРАКТЕРИСТИКИ");
+            listBox1.Items.Add($"M[X] = λT = {theorMean:F4}");
+            listBox1.Items.Add($"D[X] = λT = {theorVar:F4}");
+        }
+    }
+}
